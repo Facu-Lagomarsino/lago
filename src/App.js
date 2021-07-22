5f4c18f401ee3185c66f2d221253f67c30718ac7
Facu Lagomarsino <faculagomarsino@Facu.local>
2021-07-21 23:00:59 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg'; //no se de donde viene el error
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"lt.png"} className="App-logo" alt="logo" />
+        <p>
+          Lago' en React!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sumate!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
